8de0be8431d27baf23ba076d629251d05ede6f0e
Pablo Garcia <pgarciacamou@gmail.com>
2018-07-03 11:49:57 -0700

    add tests for forwarding reference

--- src/__tests__/index.spec.js
+++ src/__tests__/index.spec.js
@@ -3,23 +3,34 @@ import PropTypes from 'prop-types'
 import renderer from 'react-test-renderer'
 import withContext from '../'
 
-function setup() {
+function setup(propOverrides = {}) {
+  const ref = React.createRef()
+  const props = {
+    ref,
+    c: 3,
+    ...propOverrides
+  }
+
   class SomeComponent extends Component {
     static propTypes = {
       context: PropTypes.shape({
         a: PropTypes.number.isRequired,
         b: PropTypes.number.isRequired
-      }).isRequired
+      }).isRequired,
+      c: PropTypes.number.isRequired
     }
 
     render() {
-      const { context: { a, b } } = this.props
+      const { context: { a, b }, c } = this.props
       return [
         <div key='a' id='context-a'>
           {a}
         </div>,
         <div key='b' id='context-b'>
           {b}
+        </div>,
+        <div key='c' id='props-c'>
+          {c}
         </div>
       ]
     }
@@ -38,13 +49,15 @@ function setup() {
       <ContextB.Provider value={{ b: 2 }}>
         <div className='stuff'>some other content</div>
         <div className='nested element'>
-          <Consumer />
+          <Consumer {...props} />
         </div>
       </ContextB.Provider>
     </ContextA.Provider>
   )
 
   return {
+    props,
+    SomeComponent,
     ContextA,
     ContextB,
     Consumer,
@@ -56,5 +69,11 @@ describe('ContextConsumerHOC', () => {
   it('matches snapshot', () => {
     const { tree } = setup()
     expect(tree.toJSON()).toMatchSnapshot()
+  })
+
+  it('should forward the references', () => {
+    const { SomeComponent, props } = setup()
+    const { ref } = props
+    expect(ref.current.constructor).toEqual(SomeComponent)
   })
 })
