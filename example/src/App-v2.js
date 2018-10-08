bf2a45badaf32fd8b5d9267f6e7e6aa40ca577a9
Pablo Garcia <pgarciacamou@gmail.com>
2018-10-07 22:37:38 -0700

    update examples for new notation

---
+++ example/src/App-v2.js
@@ -0,0 +1,69 @@
+import React, { Component } from 'react'
+import PropTypes from 'prop-types'
+
+import { withContext } from 'react-context-consumer-hoc'
+
+// Context A
+const ContextA = React.createContext()
+function ContextAProvider(props) {
+  return (
+    <ContextA.Provider value={{ someValue: Math.random() }}>
+      {/* eslint-disable-next-line */}
+      {props.children}
+    </ContextA.Provider>
+  )
+}
+
+// Context B
+const ContextB = React.createContext()
+function ContextBProvider(props) {
+  return (
+    <ContextB.Provider value={{ someOtherValue: Math.random() }}>
+      {/* eslint-disable-next-line */}
+      {props.children}
+    </ContextB.Provider>
+  )
+}
+
+// Consumer of Context A and B
+class RandomConsumer extends Component {
+  static propTypes = {
+    someValue: PropTypes.number.isRequired,
+    someOtherValue: PropTypes.number.isRequired
+  }
+
+  constructor(props) {
+    super(props)
+
+    // do something in the state
+    this.state = {
+      color: props.someValue > 0.5 ? 'white' : 'black'
+    }
+  }
+
+  render() {
+    const { someValue, someOtherValue } = this.props
+
+    return (
+      <p>
+        Black or white? {someValue > 0.5 ? 'white' : 'black'}
+        <br />
+        This is a random number: {someOtherValue}
+      </p>
+    )
+  }
+}
+
+const WrappedConsumer = withContext(ContextA, ContextB)(RandomConsumer)
+
+export default class App extends Component {
+  render() {
+    return (
+      <ContextAProvider>
+        <ContextBProvider>
+          <WrappedConsumer />
+        </ContextBProvider>
+      </ContextAProvider>
+    )
+  }
+}
